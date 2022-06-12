ababb53b6c9b79bef195ccba53539df850169c52
boapi <tobias.weyer1205@gmail.com>
2022-06-12 07:50:19 +0200

    add handling for empty names

--- create_modules/greetings/greetings.go
+++ create_modules/greetings/greetings.go
@@ -1,11 +1,19 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // returns a greeting for the passed name
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+	// return an error if the name is empty
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	var message string
 	message = fmt.Sprintf("Hi, %v. Welcome!", name)
 
-	return message
+	return message, nil
 }
