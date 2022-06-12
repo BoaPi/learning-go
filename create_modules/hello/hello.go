a4025ed0072f8428126085c52809ccb20bc996c1
boapi <tobias.weyer1205@gmail.com>
2022-06-12 07:57:06 +0200

    add error handling

--- create_modules/hello/hello.go
+++ create_modules/hello/hello.go
@@ -2,14 +2,27 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/greetings"
 )
 
 func main() {
+	// setup of logger
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
 	// use greeting package 'Hello' function
 	var message string
-	message = greetings.Hello("Gladys")
+	var err error
+	message, err = greetings.Hello("Gladys")
 
+	// if an error was received, print to console 
+	// exit program
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// if no error was received, print greeting to the console
 	fmt.Println(message)
 }
