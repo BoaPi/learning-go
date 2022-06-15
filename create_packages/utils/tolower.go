bd20945dae032778f1415169a9684e75513c55c5
boapi <tobias.weyer1205@gmail.com>
2022-06-15 07:21:31 +0200

    apply go fmt

--- create_packages/utils/tolower.go
+++ create_packages/utils/tolower.go
@@ -4,9 +4,9 @@ import "unicode"
 
 // ToLower lowercases all the runes in its argument string.
 func ToLower(s string) string {
-    r := []rune(s)
-    for i := range r {
-        r[i] = unicode.ToLower(r[i])
-    }
-    return string(r)
+	r := []rune(s)
+	for i := range r {
+		r[i] = unicode.ToLower(r[i])
+	}
+	return string(r)
 }
