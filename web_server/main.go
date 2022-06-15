786f50c3ba8fc8c87d8905021bf541923cd694f2
boapi <tobias.weyer1205@gmail.com>
2022-06-15 07:49:41 +0200

    add album struct

--- web_server/main.go
+++ web_server/main.go
@@ -1,1 +1,10 @@
 package main
+
+// album represents data about the record album
+type album struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Artist string  `json:"artist"`
+	Price  float64 `json:"price"`
+}
+
