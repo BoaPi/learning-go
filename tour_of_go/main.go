2b1ffc08a1f557a2437d34228df26ac161eddf08
boapi <tobias.weyer1205@gmail.com>
2022-06-17 21:47:42 +0200

    add welcome lesson

---
+++ tour_of_go/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// all lessons from tour of Go bundled together
+func main() {
+	welcome()
+}
+
+// Welcome
+func welcome() {
+	fmt.Println("Welcome to the Tour of Go")
+}
