f98628e8a0bd3cfa428c1b2bf60d1ab85d8df956
boapi <tobias.weyer1205@gmail.com>
2022-06-20 07:34:06 +0200

    add basic types lesson

--- tour_of_go/main.go
+++ tour_of_go/main.go
@@ -22,6 +22,7 @@ func main() {
 		nakedReturn,
 		declareCardio,
 		multiDeclareAndInitialization,
+		basicTypes,
 	}
 
 	runLessons(lessons)
@@ -133,3 +134,9 @@ var (
 	MaxInt uint64     = 1<<64 - 1
 	z      complex128 = cmplx.Sqrt(-5 + 12i)
 )
+
+func basicTypes() {
+	fmt.Printf("Type: %T Value: %v \n", ToBe, ToBe)
+	fmt.Printf("Type: %T Value: %v \n", MaxInt, MaxInt)
+	fmt.Printf("Type: %T Value: %v \n", z, z)
+}
