23ab9dd0a43f5dc3906ea48d1d3dabe7326cb925
boapi <tobias.weyer1205@gmail.com>
2022-06-25 08:02:28 +0200

    add first array lesson

--- tour_of_go/main.go
+++ tour_of_go/main.go
@@ -44,6 +44,7 @@ func main() {
 		structLesson,
 		pointyStruct,
 		structDeclaration,
+		startingArrays,
 	}
 
 	runLessons(lessons)
@@ -451,4 +452,18 @@ func structDeclaration() {
 	)
 
 	fmt.Println(v1, v2, v3, p)
+}
+
+// some array stuff
+func startingArrays() {
+	var a [2]string
+
+	a[0] = "Hello"
+	a[1] = "World!"
+
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println("all primes until 13", primes)
 }
