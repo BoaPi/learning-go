1c3123390eb58ef633ff037aaafe02e97ba2c9b5
boapi <tobias.weyer1205@gmail.com>
2022-06-29 22:40:58 +0200

    add slice of slice lesson

--- tour_of_go/main.go
+++ tour_of_go/main.go
@@ -6,6 +6,7 @@ import (
 	"math/cmplx"
 	"math/rand"
 	"runtime"
+	"strings"
 	"time"
 )
 
@@ -52,6 +53,7 @@ func main() {
 		lengthAndCapacityOfSlice,
 		nilSlice,
 		makeSlice,
+		slicesOfSlice,
 	}
 
 	runLessons(lessons)
@@ -588,10 +590,31 @@ func makeSlice() {
 
 	b := make([]int, 0, 5)
 	printSlice("b", b)
-	
+
 	c := b[:2]
 	printSlice("c", c)
-	
+
 	d := c[2:5]
 	printSlice("d", d)
+}
+
+// slices of slice
+func slicesOfSlice() {
+	// create a tic-tac-toe board
+	board := [][]string{
+		{"-", "-", "-"},
+		{"-", "-", "-"},
+		{"-", "-", "-"},
+	}
+
+	// player turns
+	board[0][0] = "X"
+	board[2][2] = "O"
+	board[1][2] = "X"
+	board[1][0] = "O"
+	board[0][2] = "X"
+
+	for i := 0; i < len(board); i++ {
+		fmt.Printf("%s\n", strings.Join(board[i], " "))
+	}
 }
